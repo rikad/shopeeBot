ccc5c9ae7b6bc83e4a6abb644c8c30a336e84d27
rikad <rikadscreamo@gmail.com>
2020-03-08 15:45:39 +0700

    add loop by 100 per load

--- getFollowers.js
+++ getFollowers.js
@@ -2,17 +2,23 @@ const fs = require('fs');
 var shopee = require('./shopee.js');
 
 const target = process.env.TARGET;
-var s = process.env.START;
-var n = process.env.END;
+var s = parseInt(process.env.START);
+var n = parseInt(process.env.END);
 
 shopee(async function(page,browser) {
     console.log('get followers ...');
-    await page.goto(target+`?offset=${s}&limit=${n}&__classic__=1`);
 
-    const list = await page.$$eval('.btn-follow', options => options.map(option => option.textContent.trim()[0] == '+' ? option.getAttribute('shopid') : false ));
+    var list = [];
+    while (s <= n) {
+        await page.goto(target+`?offset=${s}&limit=${99}&__classic__=1`);
+        var newList = await page.$$eval('.btn-follow', options => options.map(option => option.textContent.trim()[0] == '+' ? option.getAttribute('shopid') : false ));
+        list = list.concat(newList);
+
+        s += 100
+    }
 
     fs.writeFileSync('followers.json', JSON.stringify(list));
     console.log('followers saved');
 
-    await browser.close();    
+    await browser.close();  
 });
